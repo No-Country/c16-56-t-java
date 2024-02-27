2b1acdc0e17baae69ecf9bfa390ad9562dc0cb2b
“SantiagoNoelPaguaga” <santiagopaguaga@gmail.com>
2024-02-27 18:23:20 -0300

    Login funcional

--- NoCountryGrillHouse/src/main/java/no_country_grill_house/config/SecurityConfig.java
+++ NoCountryGrillHouse/src/main/java/no_country_grill_house/config/SecurityConfig.java
@@ -57,6 +57,7 @@ public class SecurityConfig {
                 new AntPathRequestMatcher("/"),
                 new AntPathRequestMatcher("/js/*"),
                 new AntPathRequestMatcher("/css/*"),
+                new AntPathRequestMatcher("/images/**"),
                 new AntPathRequestMatcher("/images/favicon/*"),
                 new AntPathRequestMatcher("/images/Logo/*"));
     }
