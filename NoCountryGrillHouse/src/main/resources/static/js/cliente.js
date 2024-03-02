12626f7ca6fec5322f6dd7190be16c3aef8ce1e9
Canuca <cabreradavilaluisalberto@gmail.com>
2024-03-01 21:58:20 -0300

    vista cliente figma adaptacion

---
+++ NoCountryGrillHouse/src/main/resources/static/js/cliente.js
@@ -0,0 +1,9 @@
+const vistaCliente = document.querySelector('#cliente_vista');
+const vistaCarrito = document.querySelector('#carrito_vista');
+
+
+const btnCarritoVista = document-querySelector('#btn_carrito');
+btnCarritoVista.addEventListener('click', function() {
+    vistaCliente.style.display = 'none';
+    vistaCarrito.style.display = 'block';
+});
