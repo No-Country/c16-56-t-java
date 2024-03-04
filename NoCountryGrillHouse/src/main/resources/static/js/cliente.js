d60d89d88e97502ec15b3e95018e4e21238d21ea
Canuca <cabreradavilaluisalberto@gmail.com>
2024-03-04 16:25:56 -0300

    vista carrito

--- NoCountryGrillHouse/src/main/resources/static/js/cliente.js
+++ NoCountryGrillHouse/src/main/resources/static/js/cliente.js
@@ -2,7 +2,7 @@ const vistaCliente = document.querySelector('#cliente_vista');
 const vistaCarrito = document.querySelector('#carrito_vista');
 
 
-const btnCarritoVista = document-querySelector('#btn_carrito');
+const btnCarritoVista = document.querySelector('#btn_carrito');
 btnCarritoVista.addEventListener('click', function() {
     vistaCliente.style.display = 'none';
     vistaCarrito.style.display = 'block';
